4710869f7eaddfd7758a0cd6314bce82cd9e9984
Connor <cstreng95@gmail.com>
2020-07-14 17:20:58 +0200

    Updates

---
+++ precache-manifest.da5c8f8351a6251a28f9047d8e3fd952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4687fcf9ecb1d5203f444c00b90a612c",
+    "url": "/eatstagram-react/index.html"
+  },
+  {
+    "revision": "2fb3f93d1e1e902f6f3a",
+    "url": "/eatstagram-react/static/css/main.7282374c.chunk.css"
+  },
+  {
+    "revision": "3482fb12168dea6cb772",
+    "url": "/eatstagram-react/static/js/2.c163988b.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/eatstagram-react/static/js/2.c163988b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb3f93d1e1e902f6f3a",
+    "url": "/eatstagram-react/static/js/main.17737847.chunk.js"
+  },
+  {
+    "revision": "8204f2235c54cabb2a16",
+    "url": "/eatstagram-react/static/js/runtime-main.ffce09e5.js"
+  }
+]);
